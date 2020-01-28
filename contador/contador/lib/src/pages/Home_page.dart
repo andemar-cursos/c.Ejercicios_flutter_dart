88d679b1afcc1565116aa9544348800e9eb284d3
Anderson <andemartinez98@hotmail.com>
2020-01-28 16:30:12 -0500

    columnas
    - Se anadio columnas a un body
    - Se creo una variable, que se usara en los widget de Testo

--- contador/contador/lib/src/pages/Home_page.dart
+++ contador/contador/lib/src/pages/Home_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget{
+
+  //Como es un valor que no cambia y esta dentro de un statelessWidget, debe ser final.
+  final TextStyle estiloTexto = new TextStyle(fontSize: 25);
   
   //   ^^^^^ SNIPPETS= build ^^^^^^
   @override
@@ -11,7 +14,15 @@ class HomePage extends StatelessWidget{
         centerTitle: true,
       ),
       body: Center(
-        child: Text('hola mundo')),
+        child: Column(
+          //Metodo para centrar el contenido de la columna
+          mainAxisAlignment: MainAxisAlignment.center,
+          //Lista de widget que contendra la columna
+          children: <Widget>[
+            Text('Numero de click\'s', style: estiloTexto),
+            Text('0', style: estiloTexto)
+          ],
+        )),
     );
   }
 }
