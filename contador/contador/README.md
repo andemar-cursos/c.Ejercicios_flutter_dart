e590d3ca5bb8e5fbf05886c14bd8af4b181901eb
Anderson <andemartinez98@hotmail.com>
2020-01-28 15:46:17 -0500

    Hola mundo
    App la cual despliega un hola mundo

--- contador/contador/README.md
+++ contador/contador/README.md
@@ -1,6 +1,6 @@
-# flutter_app
+# contador
 
-A new Flutter application.
+A new Flutter project.
 
 ## Getting Started
 
---
+++ contador/contador/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main(){
+
+  runApp(new MyApp());
+}
+
+
+class MyApp extends StatelessWidget{
+
+  @override
+  Widget build( context ){
+    return MaterialApp(
+      home: Center(
+        child: Text('hola mundo'),
+      )
+    );
+  }
+}
