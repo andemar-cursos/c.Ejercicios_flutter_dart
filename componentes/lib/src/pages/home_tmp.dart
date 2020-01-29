598abb6d9c52f32a34e365c7fcb1570c8299876d
Anderson <andemartinez98@hotmail.com>
2020-01-29 09:26:17 -0500

    Comentarios list

--- componentes/lib/src/pages/home_tmp.dart
+++ componentes/lib/src/pages/home_tmp.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 
 class HomePageTemp extends StatelessWidget {
 
+  //----------- VARIABLES --------------\\
   final opciones = ['uno', 'dos', 'tres', 'cuatro', 'cinco'];
   final colorAzul = Color.fromARGB(255, 0, 0, 255);
 
+  //----------- BUILD --------------\\
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,13 +20,14 @@ class HomePageTemp extends StatelessWidget {
     );
   }
 
+  //----------- METODOS --------------\\
 
+  //Metodo que rellena la lista
   List<Widget> _crearItems(){
 
     List<Widget> lista = new List<Widget>();
 
-    for (String opt in opciones) {
-      
+    for (String opt in opciones) {      
       final tempWidget = ListTile(
         title: Text(opt),
       );
@@ -35,6 +38,7 @@ class HomePageTemp extends StatelessWidget {
     return lista;
   }
 
+  //Metodo que rellena la lista de manera corta
   List<Widget> _crearItemsCorto(){
     
     return opciones.map((opc){
@@ -44,8 +48,11 @@ class HomePageTemp extends StatelessWidget {
           ListTile(
             title: Text('$opc!'),
             subtitle: Text('Cualquier cosa'),
+            //Icono de la parte izquier del objeto de la lista
             leading: Icon(Icons.screen_lock_landscape),
+            //Icono de la parte derecha del objeto de la lista
             trailing: Icon(Icons.keyboard_arrow_right),
+            //Funcion al precionar un objeto de la lista
             onTap: () {},
           ),
           Divider(height: 3.0,color: colorAzul),
