2d1645558d03d254de2fd384a5ea08c7625eb1c3
Anderson <andemartinez98@hotmail.com>
2020-01-29 08:59:37 -0500

    List
    - Se creo una ListView con su contenido.

--- componentes/lib/main.dart
+++ componentes/lib/main.dart
@@ -9,12 +9,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Componentes app'),
-        ),
-        body: HomePageTemp()
-      ),
+      home: HomePageTemp(),
     );
   }
 }
--- componentes/lib/src/pages/home_tmp.dart
+++ componentes/lib/src/pages/home_tmp.dart
@@ -3,8 +3,26 @@ import 'package:flutter/material.dart';
 class HomePageTemp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Home temp'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Componentes Temp'),
+      ),
+      body: ListView(
+        children: <Widget>[
+          ListTile(
+            title: Text('LisTile title'),
+          ),
+          //widget que creara una linea entre ListTitle
+          Divider(),
+          ListTile(
+            title: Text('LisTile title'),
+          ),
+          Divider(),
+          ListTile(
+            title: Text('LisTile title'),
+          ),
+        ],
+      ),
     );
   }
 }
