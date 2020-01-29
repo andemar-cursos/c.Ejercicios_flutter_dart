b29671db00a2157113bc7a9bcb08478a11b1770a
Anderson <andemartinez98@hotmail.com>
2020-01-29 09:11:48 -0500

    Agregar contenido a List
    - Se agrego contenido a la lista del ListView

--- componentes/lib/src/pages/home_tmp.dart
+++ componentes/lib/src/pages/home_tmp.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 
 class HomePageTemp extends StatelessWidget {
+
+  final opciones = ['uno', 'dos', 'tres', 'cuatro', 'cinco'];
+  final colorAzul = Color.fromARGB(255, 0, 0, 255);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -8,21 +12,30 @@ class HomePageTemp extends StatelessWidget {
         title: Text('Componentes Temp'),
       ),
       body: ListView(
-        children: <Widget>[
-          ListTile(
-            title: Text('LisTile title'),
-          ),
-          //widget que creara una linea entre ListTitle
-          Divider(),
-          ListTile(
-            title: Text('LisTile title'),
-          ),
-          Divider(),
-          ListTile(
-            title: Text('LisTile title'),
-          ),
-        ],
+        children: _crearItems(),
       ),
     );
   }
+
+
+  List<Widget> _crearItems(){
+
+    List<Widget> lista = new List<Widget>();
+
+    for (String opt in opciones) {
+      
+      final tempWidget = ListTile(
+        title: Text(opt),
+      );
+        lista..add(tempWidget)
+             ..add(Divider(height: 3.0, color: colorAzul));
+    }
+
+    return lista;
+  }
+
+  List<Widget> _crearItemsCorto(){
+    
+  }
+
 }
