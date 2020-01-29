f4400b2d17c3830b5144b4258fc79e850299d269
Anderson <andemartinez98@hotmail.com>
2020-01-29 11:07:21 -0500

    FutureBuilder
    - Se modifico el llamado de json, por un FutureBuilder.
    - Se creo una utilidad de iconos

--- componentes/lib/src/pages/home_page.dart
+++ componentes/lib/src/pages/home_page.dart
@@ -1,5 +1,9 @@
-import 'package:componentes/src/providers/menu_provider.dart';
+//Terceros
 import 'package:flutter/material.dart';
+//Propias
+import 'package:componentes/src/providers/menu_provider.dart';
+//Utilidades
+import 'package:componentes/src/utils/icono_string_util.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -15,23 +19,46 @@ class HomePage extends StatelessWidget {
   
   
   
-  
+  //Se crea una clase que contendra una lista
   Widget _lista() {
-    print(menuProvider.opciones);
-    return ListView(
-      children: _listaItems(),
+    //Se crea un FutureBuilder debido a que se llenara mediante json
+    return FutureBuilder(
+      //Se llama al json, mediante el widget future
+      future: menuProvider.cargarData(),
+      //Se llena con una data inicial, en este caso, una lista vacia.
+      initialData: [],
+      //Se cre el builder con el contexto y con los datos de future (snapshot)
+      //Se infiere que el contex es de tipo BuildContext
+      builder: (context, AsyncSnapshot<List<dynamic>> snapshot){
+        //Se retorna un ListView
+        return ListView(
+          //Se crea la lista con los datos del snapshot
+          children: _listaItems(snapshot.data),
+        );
+
+      },
     );
   }
 
-  List<Widget> _listaItems() {
+  //Se cre un metodo con la lista de datos
+  List<Widget> _listaItems(List<dynamic> data) {
+    //variable que contendra los datos
+    final List<Widget> opciones = [];
 
-    return [
-      ListTile(title: Text('Hola mundo')),
-      Divider(),
-      ListTile(title: Text('Hola mundo')),
-      Divider(),
-      ListTile(title: Text('Hola mundo')),
-    ];
+    data.forEach((opt){
+      
+      final widgetTemp = ListTile(
+        title: Text(opt['texto']),
+        leading: getIcon(opt['icon']),
+        trailing: Icon(Icons.keyboard_arrow_right, color: Colors.blue),
+        onTap: () {},
+      );
+      
+      opciones..add(widgetTemp)
+              ..add(Divider(color: Colors.blue));
+    });
+
+    return opciones;
   }
 
 
--- componentes/lib/src/providers/menu_provider.dart
+++ componentes/lib/src/providers/menu_provider.dart
@@ -1,5 +1,6 @@
 //Importacion para obtener archivos
 import 'package:flutter/services.dart' show rootBundle;
+import 'dart:convert';
 
 //La clase queda privada, solo se mostrara su instancia
 class _MenuProvider{
@@ -7,15 +8,19 @@ class _MenuProvider{
   List<dynamic> opciones = [];
 
   _MenuProvider(){
-    _cargarData();
+    
   }
 
-  _cargarData() {
-    //Future, para cargar archivos
-    rootBundle.loadString('data/menu_opts.json')
-    .then((data) {
-      print(data);
-    });
+  //Se crea un metodo con retorno future
+  Future<List<dynamic>> cargarData() async{
+    //Se crea el llamado al json
+    final resp = await rootBundle.loadString('data/menu_opts.json');
+    //Se crea un mapa, con el jason
+    Map dataMap = json.decode(resp);
+    //Se obtiene el arreglo de rutas
+    opciones = dataMap['rutas'];
+    
+    return opciones;
   }
 
 }
