990887feff69c93cd5e2a15867ad3d68e48463e3
Anderson <andemartinez98@hotmail.com>
2020-01-29 08:46:51 -0500

    cambio de home_page
    - Se hizo un homepagetemp

--- componentes/lib/main.dart
+++ componentes/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:componentes/src/pages/home_tmp.dart';
 import 'package:flutter/material.dart';
  
 void main() => runApp(MyApp());
@@ -12,11 +13,7 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Componentes app'),
         ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
+        body: HomePageTemp()
       ),
     );
   }
