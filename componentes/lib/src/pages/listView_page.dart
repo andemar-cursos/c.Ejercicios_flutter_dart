8fea5c401189b52b0489f35fc308273244e4a42a
Anderson <andemartinez98@hotmail.com>
2020-01-30 12:14:23 -0500

    ListViewBuilder -> InfiniteScroll
    - Se agrega un scrollController al ListView
         - Se inicializa en el initState()

--- componentes/lib/src/pages/listView_page.dart
+++ componentes/lib/src/pages/listView_page.dart
@@ -7,7 +7,30 @@ class ListaPage extends StatefulWidget {
 
 class _ListaPageState extends State<ListaPage> {
 
-  List<int> _listaNumeros = [1,2,3,4,5];
+  //Permite controlar el scroll
+  ScrollController _scrollController = new ScrollController();
+
+  //Se crea una lista de numeros vacia
+  List<int> _listaNumeros = new List();
+  //Se indica el index del ultimo elemento
+  int _ultimoItem         = 0;
+
+  //Este metodo se iniciara, al momento de ingresar a la pagina
+  @override
+  void initState() { 
+    super.initState();
+    //Se agrega 10 elementos a la lista
+    _agregar10();
+    //Se crea un listener al scroll del ListView
+    _scrollController.addListener((){
+      
+     //Si el escroll llega a su ultima posicion....
+    if(_scrollController.position.pixels == _scrollController.position.maxScrollExtent){
+        //Agrega 10 elementos mas a la lista
+        _agregar10();
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,11 +45,13 @@ class _ListaPageState extends State<ListaPage> {
 
   Widget _crearLista(){
     return ListView.builder(
+      //Se crea una instancia del controlador del ListView
+      controller: _scrollController,
+      //Se indica el tamano del ListView
       itemCount: _listaNumeros.length,
       itemBuilder: (context, index){
         //Esto obtiene el numero del arreglo
         final imagen = _listaNumeros[index];
-
         return FadeInImage(
           image: NetworkImage('https://picsum.photos/500/300/?image=$imagen'),
           placeholder: AssetImage("assets/loading.gif"),
@@ -35,7 +60,14 @@ class _ListaPageState extends State<ListaPage> {
     );
   }
 
-
+  //Metodo que agrega 10 elemento mas a la lista
+  void _agregar10(){
+    for (var i = 1; i < 10; i++) {
+      _ultimoItem++;
+      _listaNumeros.add(_ultimoItem);
+    }
+    setState(() {});
+  }
 
 
 
