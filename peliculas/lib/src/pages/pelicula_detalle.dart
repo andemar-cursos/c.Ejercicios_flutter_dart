4c113452d735a221394fbdba826924a219f5f9b4
Anderson <andemartinez98@hotmail.com>
2020-01-31 16:32:09 -0500

    Diseno pagina detalle
    - Se crea un SliverList
        - Que contiene sus hijos -> delegate: SliverChildListDelegate
        - Se usa un Flexible, el cual se adapta al tamano
        -

--- peliculas/lib/src/pages/pelicula_detalle.dart
+++ peliculas/lib/src/pages/pelicula_detalle.dart
@@ -12,8 +12,31 @@ class PeliculaDetalle extends StatelessWidget {
 
     return Scaffold(
       body: CustomScrollView(
+        //Son los childrens
         slivers: <Widget>[
           _crearAppBar(pelicula),
+          //Es como el listView
+          SliverList(
+            //Los childrens
+            delegate: SliverChildListDelegate(
+              [
+                //Separacion del SliverList y el AppBar
+                SizedBox(height: 10.0),
+                _posterTitulo(context, pelicula),
+                _descripcion(pelicula),
+                _descripcion(pelicula),
+                _descripcion(pelicula),
+                _descripcion(pelicula),
+                _descripcion(pelicula),
+                _descripcion(pelicula),
+                _descripcion(pelicula),
+                _descripcion(pelicula),
+                _descripcion(pelicula),
+                _descripcion(pelicula),
+                _descripcion(pelicula),
+              ]
+            ),
+          )
         ],
       )
     );
@@ -44,9 +67,59 @@ class PeliculaDetalle extends StatelessWidget {
   }
 
 
+  Widget _posterTitulo (BuildContext context, Pelicula pelicula){
+
+    return Container(
+      //Margen en los horizontales
+      padding: EdgeInsets.symmetric(horizontal: 20.0),
+      child: Row(
+        children: <Widget>[
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20.0),
+            child: Image(
+            image: NetworkImage(pelicula.getPosterImg()),
+            height: 150.0
+            ),
+          ),
+          //Separacion ntre la imagen y los titulos
+          SizedBox(width: 20.0,),
+          //Este  es un widget que se ajusta al espacio en pantalla
+          Flexible(
+            child: Column(
+              //Est es para que quede alineado al lado izq
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                //Se pasa el titulo, el estilo y los 3 puntos, si el titulo es muy largo
+                Text(pelicula.title, style: Theme.of(context).textTheme.title, overflow: TextOverflow.ellipsis),
+                Text(pelicula.originalTitle, style: Theme.of(context).textTheme.subhead, overflow: TextOverflow.ellipsis),
+                //Separacion entre los titulos y la calificacion
+                SizedBox(height: 20),
+                Row(
+                  children: <Widget>[
+                    Icon(Icons.star_border),
+                    Text(pelicula.voteAverage.toString(), style: Theme.of(context).textTheme.subhead),
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
 
 
+  Widget _descripcion (Pelicula pelicula){
 
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),      
+      child: Text(
+        pelicula.overview,
+        //Esto sirve para justificar el texto
+        textAlign: TextAlign.justify,
+      ),
+    );
+  }
 
 
 
