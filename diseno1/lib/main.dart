ef4c7775dfd3f512b275784f09a2aae0b7f180d2
Anderson <andemartinez98@hotmail.com>
2020-02-03 12:27:11 -0500

    pageView (pagina1 - pagina 2)
    - Se creo dos widget's, dentro de pageView.
        - Se uso l a propiedad scrollDirection para desplazarse arriba y
          abajo entre paginas

--- diseno1/lib/main.dart
+++ diseno1/lib/main.dart
@@ -1,5 +1,6 @@
 //Para hacer el metodo base se usa 'mateapp'
 import 'package:diseno1/src/pages/basico_page.dart';
+import 'package:diseno1/src/pages/scroll_page.dart';
 import 'package:flutter/material.dart';
  
 void main() => runApp(MyApp());
@@ -10,10 +11,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Disenos',
-      initialRoute: 'basico',
+      initialRoute: 'scroll',
       routes: {
         'basico'  : (BuildContext context) => BasicoPage(),
-
+        'scroll'  : (BuildContext context) => ScrollPage(),
       }
     );
   }
