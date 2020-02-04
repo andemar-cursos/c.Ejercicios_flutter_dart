aed74c5603a97bfee9a9dad2fed98bea62cd790f
Anderson <andemartinez98@hotmail.com>
2020-02-04 14:48:23 -0500

    Borrar registros
    - Se hace el llamado a delteScan, mediante el atributo onDismissed del widget Dismissible, que permite mover los items a izq o der.

--- qrscanner/qrscanner/lib/src/pages/mapa_page.dart
+++ qrscanner/qrscanner/lib/src/pages/mapa_page.dart
@@ -7,22 +7,48 @@ class MapasPage extends StatelessWidget {
     return FutureBuilder(
       future: DBProvider.db.getScanAll(),
       builder: (BuildContext context, AsyncSnapshot<List<ScanModel>> snapshot) {
-        
+        //Retorna un loading, mientras obtiene datos
         if(!snapshot.hasData) return Center(child: CircularProgressIndicator(),);
-
+        //Al obtener los datos, se guardan en scans
         final scans = snapshot.data;
-
+        //Se verifica que tenga algun elemento
         if(scans.length == 0) return Center(child: Text("No hay informacion"));
-
-        return ListView.builder(
-          itemCount: scans.length,
-          itemBuilder: (context, i) => ListTile(
-            leading: Icon(Icons.cloud_queue,  color: Theme.of(context).primaryColor),
-            title: Text(scans[i].valor),
-            trailing: Icon(Icons.keyboard_arrow_right, color: Colors.grey),
-          ),
-        );
+        //Se retorna un ListView con los elementos obtenidos
+        return _crearListaElementos(scans);
       },
     );
   }
+
+
+  Widget _crearListaElementos(List<ScanModel> scans){
+    return ListView.builder(
+          itemCount: scans.length,
+          itemBuilder: (context, i) => Dismissible(
+            //key aleatoria, para saber que item se selecciono
+            key: UniqueKey(),
+            //Esto cambia el color del item cuando se mueve
+            background: Container(color: Colors.red),
+            onDismissed: (direccion) => DBProvider.db.deleteScan(scans[i].id),
+            child: ListTile(
+              leading: Icon(Icons.cloud_queue,  color: Theme.of(context).primaryColor),
+              title: Text(scans[i].valor),
+              subtitle: Text("ID: ${scans[i].id.toString()}"),
+              trailing: Icon(Icons.keyboard_arrow_right, color: Colors.grey),
+            ),
+          ),
+        );
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
