44bbee93340d8f1474e0050ac465931e1f42ad7e
Anderson <andemartinez98@hotmail.com>
2020-02-04 17:13:13 -0500

    Creacion del mapa
    - Se inserto el widget del mapa 'FlutterMap'.

--- qrscanner/qrscanner/lib/src/models/scan_model.dart
+++ qrscanner/qrscanner/lib/src/models/scan_model.dart
@@ -1,3 +1,6 @@
+//Terceros
+import 'package:latlong/latlong.dart';
+
 class ScanModel {
   int id;
   String tipo;
@@ -25,4 +28,15 @@ class ScanModel {
       "tipo"  : tipo,
       "valor" : valor,
   };
+
+  //Retorna la Latitud y longitud con tipo LatLng
+  LatLng getLatLng(){
+    //geo:3.4517276203872265,-76.5467120691193
+    //[3.4517276203872265, -76.5467120691193]
+    final lalo = valor.substring(4).split(',');
+    final lat = double.parse(lalo[0]);
+    final lng = double.parse(lalo[1]);
+
+    return LatLng(lat, lng);
+  }
 }
--- qrscanner/qrscanner/lib/src/pages/mapa_page.dart
+++ qrscanner/qrscanner/lib/src/pages/mapa_page.dart
@@ -1,5 +1,7 @@
 //terceros
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong/latlong.dart';
 //modelos
 import 'package:qrscanner/src/models/scan_model.dart';
 
@@ -20,9 +22,39 @@ class MapaPage extends StatelessWidget {
           )
         ],
       ),
-      body: Center(
-        child: Text(scan.valor),
-      )
+      body: _crearFlutterMap(scan),
     );
   }
+
+
+  //Widget
+  Widget _crearFlutterMap(ScanModel scan){
+
+    return FlutterMap(
+      options: MapOptions(
+        center: scan.getLatLng(),
+        zoom: 15,
+      ),
+      layers: [
+        _crearMapa(),
+      ],
+    );
+  }
+
+
+  LayerOptions _crearMapa(){
+    
+    return TileLayerOptions(
+      urlTemplate: 'https://api.mapbox.com/v4/'
+      '{id}/{z}/{x}/{y}@2x.png?access_token={accessToken}',
+      additionalOptions: {
+        'accessToken' : 'pk.eyJ1IjoiYW5kZW1hciIsImEiOiJjamhlMWs0YXUwdWMxM2FvMTFoc2xoY2VkIn0.kA78PqrsSBpILUiuQXkojA',
+        'id'          : 'mapbox.streets'
+      } 
+    );
+  }
+
+
+
+
 }
