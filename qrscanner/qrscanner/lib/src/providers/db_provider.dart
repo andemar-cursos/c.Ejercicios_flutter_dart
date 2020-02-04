13ff9d054bae23e14e7a3b35961b6e893908efb7
Anderson <andemartinez98@hotmail.com>
2020-02-04 12:08:40 -0500

    Creando DBLocal parte3
    - Se crearon los metodos de insert, uno manual y otro 'automatico'

--- qrscanner/qrscanner/lib/src/providers/db_provider.dart
+++ qrscanner/qrscanner/lib/src/providers/db_provider.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:qrscanner/src/models/scan_model.dart';
 import 'package:sqflite/sqflite.dart';
 
 class DBProvider {
@@ -42,8 +43,35 @@ class DBProvider {
         );
       }
     );
-
   }
+
+  //Crear registros de base de datos -> Metodo de insercion Manual
+  nuevoScanRaw(ScanModel nuevoScan) async{
+
+    final db = await database;
+
+    final res = await db.rawInsert(
+      """
+      INSERT INTO Scans (id, tipo, valor) 
+      VALUES (${nuevoScan.id}, '${nuevoScan.tipo}', '${nuevoScan.valor}')
+      """
+    );
+
+    //Numero de inserciones realizadas
+    return res;
+  }
+
+
+  nuevoScan(ScanModel nuevoScan) async{
+
+    final db = await database;
+
+    final res = db.insert('Scans', nuevoScan.toJson());
+
+    return res;
+  }
+
+
 
 
 
