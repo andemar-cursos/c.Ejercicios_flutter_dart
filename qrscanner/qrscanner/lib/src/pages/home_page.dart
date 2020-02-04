5ba1790da78ae689b4e75545d9cc2a583fdbd008
Anderson <andemartinez98@hotmail.com>
2020-02-04 09:21:32 -0500

    AppBarr cambio de pagina
    - Se creo una propiedad (_currentIndex) que guarda la ubicacion del usuario.

--- qrscanner/qrscanner/lib/src/pages/home_page.dart
+++ qrscanner/qrscanner/lib/src/pages/home_page.dart
@@ -1,12 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:qrscanner/src/pages/direcciones_page.dart';
+import 'package:qrscanner/src/pages/mapa_page.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+
+  //Build
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  int _currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("HomePage"),
-      ),
+      body: _callPage(_currentIndex),
+      bottomNavigationBar: _crearBottonNavigatorBar(),
     );
+  }
+
+  Widget _crearBottonNavigatorBar(){
+
+    return BottomNavigationBar(
+      //Que elemento esta activo
+      currentIndex: _currentIndex,
+      //Recibe el index, el cual se oprimio
+      onTap: (index){
+        setState(() {
+          _currentIndex = index;
+        });
+      },
+      items: [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.map),
+          title: Text("mapas")
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.brightness_5),
+          title: Text("direcciones")
+        ),
+      ],
+    );
+  }
+
+  Widget _callPage(int paginaActual){
+    switch(paginaActual){
+      case 0: return MapasPage();
+      case 1: return DireccionesPage();
+      default:return MapasPage();
+    }
   }
 }
