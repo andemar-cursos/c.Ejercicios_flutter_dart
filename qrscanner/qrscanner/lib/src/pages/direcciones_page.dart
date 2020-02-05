1522e1c474de748747c67f6dd284a8b9deea786b
Anderson <andemartinez98@hotmail.com>
2020-02-05 09:54:34 -0500

    Creacion de direcciones_page
    Simplemente se copio el contenido de mapas_page, se cambio el nombre de la clase y el stream.

--- qrscanner/qrscanner/lib/src/pages/direcciones_page.dart
+++ qrscanner/qrscanner/lib/src/pages/direcciones_page.dart
@@ -1,10 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:qrscanner/src/bloc/scans_bloc.dart';
+import 'package:qrscanner/src/providers/db_provider.dart';
+//Utils
+import 'package:qrscanner/src/utils/utils.dart' as utils;
 
 class DireccionesPage extends StatelessWidget {
+
+  final scanBloc = new ScansBlock();
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Direcciones page"),
+    //Se hace el llamado, cada vez que se ingresa a la pagina
+    scanBloc.obtenerScans();
+
+    return StreamBuilder<List<ScanModel>>(
+      stream: scanBloc.scansStreamHttp,
+      builder: (BuildContext context, AsyncSnapshot<List<ScanModel>> snapshot) {
+        //Retorna un loading, mientras obtiene datos
+        if(!snapshot.hasData) return Center(child: CircularProgressIndicator(),);
+        //Al obtener los datos, se guardan en scans
+        final scans = snapshot.data;
+        //Se verifica que tenga algun elemento
+        if(scans.length == 0) return Center(child: Text("No hay informacion"));
+        //Se retorna un ListView con los elementos obtenidos
+        return _crearListaElementos(scans);
+      },
     );
   }
+
+
+  Widget _crearListaElementos(List<ScanModel> scans){
+    return ListView.builder(
+          itemCount: scans.length,
+          itemBuilder: (context, i) => Dismissible(
+            //key aleatoria, para saber que item se selecciono
+            key: UniqueKey(),
+            //Esto cambia el color del item cuando se mueve
+            background: Container(color: Colors.red),
+            onDismissed: (direccion) => scanBloc.borrarScan(scans[i].id),
+            child: ListTile(
+              leading: Icon(Icons.cloud_queue,  color: Theme.of(context).primaryColor),
+              title: Text(scans[i].valor),
+              subtitle: Text("ID: ${scans[i].id.toString()}"),
+              trailing: Icon(Icons.keyboard_arrow_right, color: Colors.grey),
+              onTap: () => utils.abrirScan(context, scans[i]),
+            ),
+          ),
+        );
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
