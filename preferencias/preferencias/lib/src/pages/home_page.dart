137aa11894c5320e005e0ba3c9626d2511e29017
Anderson <andemartinez98@hotmail.com>
2020-02-05 12:12:37 -0500

    Crear drawer
    - Construccion de drawer (Menu latreal)

--- preferencias/preferencias/lib/src/pages/home_page.dart
+++ preferencias/preferencias/lib/src/pages/home_page.dart
@@ -1,4 +1,7 @@
+//Terceros
 import 'package:flutter/material.dart';
+//Paginas
+import 'package:preferencias/src/pages/settings_page.dart';
 
 class HomePage extends StatelessWidget {
 
@@ -10,6 +13,8 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Preferencias'),
       ),
+      //Esto permite desplegar un menu lateral
+      drawer: _crearMenu(context),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
@@ -23,4 +28,58 @@ class HomePage extends StatelessWidget {
       ),
     );
   }
+
+
+  //Widget's
+  Drawer _crearMenu(BuildContext context){
+
+    return Drawer(
+      child: ListView(
+        //Esto hace que comience desde el inicio de la pantalla (incluyendo el area de noficicaciones)
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            child: Container(),
+            decoration: BoxDecoration(
+              image: DecorationImage(
+               image: AssetImage('assets/img/menu.jpg'),
+               fit: BoxFit.cover
+              )  
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.home, color: Theme.of(context).primaryColor),
+            title: Text('Home'),
+            onTap: () => Navigator.pushReplacementNamed(context, HomePage.routeName),
+          ),
+          
+          ListTile(
+            leading: Icon(Icons.party_mode, color: Theme.of(context).primaryColor),
+            title: Text('Party mode'),
+            onTap: (){},
+          ),
+          
+          ListTile(
+            leading: Icon(Icons.people, color: Theme.of(context).primaryColor),
+            title: Text('People'),
+            onTap: (){},
+          ),
+          
+          ListTile(
+            leading: Icon(Icons.settings, color: Theme.of(context).primaryColor),
+            title: Text('Preferencias'),
+            onTap: () => Navigator.pushReplacementNamed(context, SettingsPage.routeName),
+          ),
+          
+        ],
+      ),
+    );
+  }
+
+
+
+
+
+
+
 }
