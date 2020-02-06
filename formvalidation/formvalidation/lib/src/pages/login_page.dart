e542cfee97d6800e607e0d47716f0972ac406bc1
Anderson <andemartinez98@hotmail.com>
2020-02-06 10:07:10 -0500

    Creacion de la caja elevada, de los inputs
    Se uso SingleChildScrollView para que el input, puda moverse cuando no tiene espacio.

--- formvalidation/formvalidation/lib/src/pages/login_page.dart
+++ formvalidation/formvalidation/lib/src/pages/login_page.dart
@@ -4,18 +4,21 @@ class LoginPage extends StatelessWidget {
 
   static final String routeName = 'LoginPage';
 
+  //Build
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
       body: Stack(
         children: <Widget>[
           _crearFondo(context),
+          _loginForm(context),
         ],
       )
     );
   }
+  //-----
 
-  //Widget's
+  //Widget fondo
   Widget _crearFondo(BuildContext context){
     //tamano de la pantalla
     final size = MediaQuery.of(context).size;
@@ -23,7 +26,9 @@ class LoginPage extends StatelessWidget {
     final fondoModaro = Container(
       //Este es el 40% de la pantalla
       height: size.height * 0.4,
+      //Coge el total del ancho
       width: double.infinity,
+      //Para crear un gradiente lineal
       decoration: BoxDecoration(
         gradient: LinearGradient(
           colors: <Color>[
@@ -67,12 +72,125 @@ class LoginPage extends StatelessWidget {
       ],
     );
   }
+  //--------
 
+  //Widget Login
+  Widget _loginForm(BuildContext context){
 
+    final size = MediaQuery.of(context).size;
 
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          //Este SafeArea mueve los elementos a un area seguro, mas 230 pixeles
+          SafeArea(
+            child: Container(
+              height: 230,
+            ),
+          ),
+          _cajaLogin(size),
+          Text('¿Olvido la contraseña?'),
+          //Esto permite que la caja no quede pegada en la esquina, cuando la pantalla esta en horizontal
+          SizedBox(height: 100.0),
+        ],
+      ),
+    );
+  }
+  Widget _cajaLogin(Size size){
 
+    return Container(
+      //La caja sera del 85% del ancho de la pantalla
+      width: size.width * 0.85,
+      //Este me separar de los elementos exteriores de la caja
+      margin: EdgeInsets.symmetric(vertical: 30.0),
+      //Este separa los elementos interiores de la caja
+      padding: EdgeInsets.symmetric(vertical: 50.0),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(5.0),
+        boxShadow: <BoxShadow>[
+          BoxShadow(
+            //Esto indica el color de la sombre (Opacidad de 26)
+            color: Colors.black26,
+            //Este el blur que contendra la sombra
+            blurRadius: 3.0,
+            //Este expandira mas el blue
+            spreadRadius: 3.0,
+            //Este la posicion que tendra la sombra
+            offset: Offset(0.0, 5.0),
+          )
+        ]
+      ),
+      child: Column(
+        children: <Widget>[
+          Text('Ingreso', style: TextStyle(fontSize: 20.0),),
+          SizedBox(height: 40.0),
+          _crearEmail(),
+          SizedBox(height: 20.0),
+          _crearPass(),
+          SizedBox(height: 20.0),
+          _crearBoton(),
+        ],
+      ),
+    ); 
+  }
+  Widget _crearEmail(){
 
+    return Container(
+      //Esto permite separar la caja de los bordes
+      padding: EdgeInsets.symmetric(horizontal: 20.0),
+      //Label del correo
+      child: TextField(
+        //Al desplegar el teclado, es de tipo email
+        keyboardType: TextInputType.emailAddress,
+        decoration: InputDecoration(
+          icon: Icon(Icons.alternate_email, color: Colors.deepPurple),
+          //Este es el texto, al oprimir el label
+          hintText: 'ejemplo@correo.com',
+          //Estees el texto, cuando no se ha oprimido el label
+          labelText: 'Correo Electronico',
+        ),
+      ),
+    );
+  }
+  Widget _crearPass(){
 
+    return Container(
+      //Esto permite separar la caja de los bordes
+      padding: EdgeInsets.symmetric(horizontal: 20.0),
+      //Label del pass
+      child: TextField(
+        //Permite ocultar la pass cuando se escribe
+        obscureText: true,
+        decoration: InputDecoration(
+          icon: Icon(Icons.lock_outline, color: Colors.deepPurple),
+          labelText: 'Contrasena',
+        ),
+      ),
+    );
+  }
+  Widget _crearBoton(){
+    //Es un boton con el tema basado en material
+    return RaisedButton(
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 80.0, vertical: 15.0),
+        child: Text('Ingresar'),
+      ),
+      shape: RoundedRectangleBorder(
+        //Bordes redondeados del boton
+        borderRadius: BorderRadius.circular(5.0)
+      ),
+      //Esto evita que el boton tenga sombra
+      elevation: 0.0,
+      //Color del boton
+      color: Colors.deepPurple,
+      //Color del texto
+      textColor: Colors.white,
+      //Funcion
+      onPressed: (){},
+    );
+  }
+  //----
 
 
 
