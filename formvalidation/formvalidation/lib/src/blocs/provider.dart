acdf5fd301267050e919c0c71fd77ed7241769ea
Anderson <andemartinez98@hotmail.com>
2020-02-06 14:06:08 -0500

    Convertir el provider un singleton

--- formvalidation/formvalidation/lib/src/blocs/provider.dart
+++ formvalidation/formvalidation/lib/src/blocs/provider.dart
@@ -4,10 +4,22 @@ export 'package:formvalidation/src/blocs/login_bloc.dart';
 
 class Provider extends InheritedWidget{
 
-  final loginBloc = LoginBloc();
+  static Provider _instancia;
 
-  Provider({Key key, Widget child})
+  factory Provider({Key key, Widget child}){
+
+    if( _instancia == null){
+      _instancia = new Provider._internal(key: key, child: child);
+    }
+
+    return _instancia;
+  }
+
+    Provider._internal({Key key, Widget child})
     : super(key:key, child:child);
+
+
+  final loginBloc = LoginBloc();
 
   //Esto permite notificar el 100% de los llamados del Stream.
   @override
