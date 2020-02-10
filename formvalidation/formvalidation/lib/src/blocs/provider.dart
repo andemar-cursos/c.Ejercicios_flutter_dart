c1d11813208c1d0796713e002f4e42c69d71264e
Anderson <andemartinez98@hotmail.com>
2020-02-10 15:03:23 -0500

    provider -> ProductoBloc
    - Se anadio la instancia de productoBloc al provider.

--- formvalidation/formvalidation/lib/src/blocs/provider.dart
+++ formvalidation/formvalidation/lib/src/blocs/provider.dart
@@ -1,8 +1,16 @@
+//Terceros
 import 'package:flutter/material.dart';
+//Bloc
 import 'package:formvalidation/src/blocs/login_bloc.dart';
 export 'package:formvalidation/src/blocs/login_bloc.dart';
+import 'package:formvalidation/src/blocs/productos_bloc.dart';
+export 'package:formvalidation/src/blocs/productos_bloc.dart';
 
 class Provider extends InheritedWidget{
+  //Blocs
+  final loginBloc = LoginBloc();
+  //Se debe declarar privado
+  final _productosbloc = new ProductosBloc();
 
   static Provider _instancia;
 
@@ -19,7 +27,6 @@ class Provider extends InheritedWidget{
     : super(key:key, child:child);
 
 
-  final loginBloc = LoginBloc();
 
   //Esto permite notificar el 100% de los llamados del Stream.
   @override
@@ -29,6 +36,16 @@ class Provider extends InheritedWidget{
   static LoginBloc of(BuildContext context){
     return ( context.inheritFromWidgetOfExactType(Provider) as Provider ).loginBloc;
   }
+
+  static ProductosBloc productosBloc(BuildContext context){
+    return ( context.inheritFromWidgetOfExactType(Provider) as Provider )._productosbloc;
+  }
+
+
+
+
+
+
 
 
 
