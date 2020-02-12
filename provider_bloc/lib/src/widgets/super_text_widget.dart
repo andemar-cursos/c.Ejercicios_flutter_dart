c41f3c3e073dd49197855a00c55b217386274403
Anderson <andemartinez98@hotmail.com>
2020-02-12 12:22:49 -0500

    Uso de provider sin el context
    - Para el uso del provider sin el context de la clase, se puede usar el widget Consumer, el cual hace un builder que trae un context local.

--- provider_bloc/lib/src/widgets/super_text_widget.dart
+++ provider_bloc/lib/src/widgets/super_text_widget.dart
@@ -4,18 +4,27 @@ import 'package:provider_bloc/src/providers/animes_info_provider.dart';
 
 class SuperText extends StatelessWidget {
 
-  final estiloTexto = TextStyle(fontSize: 30.0, color: Colors.blueAccent);
+  //final estiloTexto = TextStyle(fontSize: 30.0, color: Colors.blueAccent);
 
   @override
   Widget build(BuildContext context) {
     
     //Intancia del provider
-    final animeInfo = Provider.of<AnimesInfo>(context);
+    //final animeInfo = Provider.of<AnimesInfo>(context);
+
+
 
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
-        Text(animeInfo.anime, style: estiloTexto),
+        //Con este consumer, nos permite evitar el uso del context de la clase
+        Consumer<AnimesInfo>(
+          builder: (context, animeInfo, _) => 
+          Text(
+            animeInfo.anime,
+            style: TextStyle(fontSize: 30.0, color: animeInfo.colorBase)
+          ),
+        ),
       ],
     );
   }
