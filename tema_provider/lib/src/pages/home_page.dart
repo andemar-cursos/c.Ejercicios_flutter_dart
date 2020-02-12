4b4d225b14016d2a78f405e8e2a371051512504a
Anderson <andemartinez98@hotmail.com>
2020-02-12 12:55:44 -0500

    Initial commit -> tema_provider

---
+++ tema_provider/lib/src/pages/home_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home page'),
+      ),
+      body: Center(
+        child: 
+        Text('Home page'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: (){},
+      ),
+    );
+  }
+}
